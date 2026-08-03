5726d7bff2bd8043469927a702f689bc79da99a2
HaydenChung <chunghayden@gmail.com>
2026-08-03 19:14:26 +1200

    Starting programming of first version

---
+++ career_planning_program_v1.py
@@ -0,0 +1,1 @@
+#this is the first verion of my career planning program. It is designed to help users identify their career goals, assess their skills and interests, and create a personalized career plan.

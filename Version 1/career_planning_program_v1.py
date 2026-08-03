1cc0e210e25afe67b4f907cbbcc6a20e817aae34
HaydenChung <chunghayden@gmail.com>
2026-08-03 19:28:25 +1200

    Created class blueprint for careers

--- Version 1/career_planning_program_v1.py
+++ Version 1/career_planning_program_v1.py
@@ -1,1 +1,20 @@
 #this is the first verion of my career planning program. It is designed to help users identify their career goals, assess their skills and interests, and create a personalized career plan.
+
+#creating class for careers
+class Career:
+    def __init__(self, name, sector, salary, description, education_required):
+        self.name = name
+        self.sector = sector
+        self.salary = salary
+        self.description = description
+        self.education_required = education_required
+
+    def display_info(self):
+        print("================= Career Information ================")
+        print(f"Career: {self.name}")
+        print(f"Sector: {self.sector}")
+        print(f"Salary: ${self.salary}")
+        print(f"Description: {self.description}")
+        print(f"Education Required: {self.education_required}")
+        print("=====================================================\n\n")
+

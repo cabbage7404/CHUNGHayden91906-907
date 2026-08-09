2487ff08217f51b40879a48f14b367a20728f56b
HaydenChung <chunghayden@gmail.com>
2026-08-10 11:47:48 +1200

    Added career class from version 1

--- Version 2/career_planning_program_v2.py
+++ Version 2/career_planning_program_v2.py
@@ -4,3 +4,22 @@
 import json
 import os
 
+#creating class for careers
+class Career:
+       #initializing career attributes
+       def __init__(self, name, sector, salary, description, education_required):
+              self.name = name
+              self.sector = sector
+              self.salary = salary
+              self.description = description
+              self.education_required = education_required
+
+       #function to display career information
+       def display_info(self):
+              print("================= Career Information ================")
+              print(f"Career: {self.name}")
+              print(f"Sector: {self.sector}")
+              print(f"Salary: ${self.salary}")
+              print(f"Description: {self.description}")
+              print(f"Education Required: {self.education_required}")
+              print("=====================================================\n\n")

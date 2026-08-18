e4c9cd4ae851ea3d58ce5678f593b7d8a276e4fa
HaydenChung <chunghayden@gmail.com>
2026-08-18 13:20:21 +1200

    Created function to load questions from external file and shorted name for load careers function

--- Version 3/career_planning_program_v3.py
+++ Version 3/career_planning_program_v3.py
@@ -6,8 +6,20 @@ import hashlib
 import tkinter as tk
 from tkinter import messagebox, ttk
 
-def load_careers_from_file():
+def load_careers():
     with open("careers.json", "r") as file:
         data = json.load(file)
     return data
+
+def load_questions():
+    try:
+        with open("quiz_questions.json", "r") as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        messagebox.showerror("Error", "quiz_questions.json not found")
+        return []
+    except json.JSONDecodeError:
+        messagebox.showerror("Error", "quiz_questions.json is corrupted")
+        return []
 

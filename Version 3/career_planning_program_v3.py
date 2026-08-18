1941f89c07c909bda874e745942eb072694b2090
HaydenChung <chunghayden@gmail.com>
2026-08-18 13:15:47 +1200

    Setting up Python and JSON files, as well as importing modules to be used in Python

---
+++ Version 3/career_planning_program_v3.py
@@ -0,0 +1,8 @@
+#this is the third version of my program, which has more functions and features as well as built in GUI
+
+import json
+import os
+import hashlib
+import tkinter as tk
+from tkinter import messagebox, ttk
+
